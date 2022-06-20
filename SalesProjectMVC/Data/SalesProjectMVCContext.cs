4d2ed4b9a8a332f791b87c3522f0266c16d18bdc
mignnoni <iurimig@gmail.com>
2022-06-19 22:36:00 -0300

    Creating exceptions

---
+++ SalesProjectMVC/Data/SalesProjectMVCContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SalesProjectMVC.Models;
+
+namespace SalesProjectMVC.Data
+{
+    public class SalesProjectMVCContext : DbContext
+    {
+        public SalesProjectMVCContext (DbContextOptions<SalesProjectMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+    }
+}
---
+++ SalesProjectMVC/Services/SellerService.cs
@@ -0,0 +1,62 @@
+﻿using SalesProjectMVC.Data;
+using SalesProjectMVC.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SalesProjectMVC.Services.Exceptions;
+
+namespace SalesProjectMVC.Services
+{
+    public class SellerService
+    {
+        private readonly SalesProjectMVCContext _context;
+
+        public SellerService(SalesProjectMVCContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(seller => seller.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
+            _context.SaveChanges();
+
+        }
+
+        public void Update(Seller seller)
+        {
+            if(!_context.Seller.Any(x => x.Id == seller.Id))
+            {
+                throw new NotFoundException("Seller not found");
+            }
+
+            try
+            {
+                _context.Update(seller);
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
+
+        }
+    }
+}
